9a4f76d653af5dad31c779e96f058118c46f07f4
RimiNosha <riminosha@gmail.com>
2025-07-31 17:58:49 +0100

    Update stitcher

--- tools/floor_tile_stitch/stitch.py
+++ tools/floor_tile_stitch/stitch.py
@@ -5,23 +5,40 @@ import PIL.PngImagePlugin
 
 # Some lazy code inbound - RimiNosha
 
+dmiData = """# BEGIN DMI
+version = 4.0
+	width = 32
+	height = 32
+state = "tile_center"
+	dirs = 1
+	frames = 1
+state = "tile_corner"
+	dirs = 4
+	frames = 1
+state = "tile_edge"
+	dirs = 4
+	frames = 1
+state = "tile_center_inverted"
+	dirs = 1
+	frames = 1
+state = "tile_corner_inverted"
+	dirs = 4
+	frames = 1
+# END DMI"""
+
 if len(sys.argv) < 2:
     print("You need to supple an image via argument (or drop it on the script)")
     sys.exit(0)
 
-template = PIL.Image.open("template.dmi", formats=["png"])
 tiles = PIL.Image.open(sys.argv[1])
 tileA = PIL.Image.new("RGBA", (16, 16), (0, 0, 0, 0))
 tileA.paste(tiles)
 tileB = PIL.Image.new("RGBA", (16, 16), (0, 0, 0, 0))
 tileB.paste(tiles, (-16, 0))
-newImage = PIL.Image.new("RGBA", (96, 96), (0, 0, 0, 0))
+newImage = PIL.Image.new("RGBA", (96, 160), (0, 0, 0, 0))
 
 pngInfo = PIL.PngImagePlugin.PngInfo()
-pngInfo.add_text("Description", template.info["Description"], zip=True)
-
-# tileA.save("tilea.png", "png") # Debug
-# tileB.save("tileb.png", "png") # Debug
+pngInfo.add_text("Description", dmiData, zip=True)
 
 tiles = {"a": tileA, "b": tileB}
 
@@ -31,15 +48,23 @@ locToLayout = {
     (2, 0): ["b", "a", "a", "a"],
     (0, 1): ["a", "b", "a", "a"],
     (1, 1): ["a", "a", "b", "a"],
-    (2, 1): ["a", "a", "a", "b"],
-    (0, 2): ["b", "a", "a", "a"],
-    (1, 2): ["a", "b", "a", "a"],
-    (2, 2): ["a", "a", "b", "a"],
+    (2, 1): ["a", "a", "b", "b"],
+    (0, 2): ["b", "b", "a", "a"],
+    (1, 2): ["a", "b", "a", "b"],
+    (2, 2): ["b", "a", "b", "a"],
+    (0, 3): ["b", "b", "b", "b"],
+    (1, 3): ["b", "b", "b", "a"],
+    (2, 3): ["a", "b", "b", "b"],
+    (0, 4): ["b", "a", "b", "b"],
+    (1, 4): ["b", "b", "a", "b"],
 }
 
-for yLoc in range(3):
+for yLoc in range(5):
     for xLoc in range(3):
         floor = PIL.Image.new("RGBA", (96, 96), (0, 0, 0, 0))
+
+        if not (xLoc, yLoc) in locToLayout:
+            continue
 
         tileStrs = locToLayout[(xLoc, yLoc)]
 
@@ -51,21 +76,3 @@ for yLoc in range(3):
         newImage.paste(floor, (xLoc * 32, yLoc * 32))
 
 newImage.save(pathlib.Path(sys.argv[1]).stem + ".dmi", "png", pnginfo=pngInfo)
-
-tiles = {"a": tileB, "b": tileA}
-
-for yLoc in range(3):
-    for xLoc in range(3):
-        floor = PIL.Image.new("RGBA", (96, 96), (0, 0, 0, 0))
-
-        tileStrs = locToLayout[(xLoc, yLoc)]
-
-        floor.paste(tiles[tileStrs[0]])
-        floor.paste(tiles[tileStrs[1]], (16, 0))
-        floor.paste(tiles[tileStrs[2]], (0, 16))
-        floor.paste(tiles[tileStrs[3]], (16, 16))
-
-        newImage.paste(floor, (xLoc * 32, yLoc * 32))
-
-newImage.save(pathlib.Path(sys.argv[1]).stem + "_inverted.dmi", "png", pnginfo=pngInfo)
-print(template.info["Description"])
