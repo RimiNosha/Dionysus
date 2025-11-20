6013f3726d4ec016b6efbd0ab1a5f5f03e8dceb9
RimiNosha <riminosha@gmail.com>
2025-11-20 17:32:24 +0000

    Fuck you *implodes your tooling*

--- tools/sass/build.py
+++ tools/sass/build.py
@@ -5,4 +5,4 @@ from sass_embedded import compile_directory
 from pathlib import Path
 
 print("Compiling " + os.path.realpath("../nano/scss"))
-compile_directory(Path("../nano/scss"), dest=Path("../nano/css"), embed_sourcemap=True)
+compile_directory(Path("../nano/scss"), dest=Path("../nano/css"), embed_sourcemap=True, style="expanded" if os.getenv("DEBUG") else "compressed")
