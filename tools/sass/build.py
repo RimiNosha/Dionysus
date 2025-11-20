7c7ccfaec635efe409ec08bcb31e42e4e4cf90f6
RimiNosha <riminosha@gmail.com>
2025-11-20 17:17:51 +0000

    And so begins the ass blasting of rimiui or whatever I'm gonna call it

---
+++ tools/sass/build.py
@@ -0,0 +1,8 @@
+import os.path
+import sys
+
+from sass_embedded import compile_directory
+from pathlib import Path
+
+print("Compiling " + os.path.realpath("../nano/scss"))
+compile_directory(Path("../nano/scss"), dest=Path("../nano/css"), embed_sourcemap=True)
